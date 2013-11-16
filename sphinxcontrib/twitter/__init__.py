7592d95f3af2fc3a27f4d8ebdd267b9c5b9da64b
shomah4a <shoma.h4a+github@gmail.com>
2013-11-16 11:49:46 +0900

    version up to 0.3.0

--- sphinxcontrib/twitter/__init__.py
+++ sphinxcontrib/twitter/__init__.py
@@ -19,6 +19,8 @@ then use `tweet` directive and `tw` role.
 
    :tw:`@shomah4a`
 
+   .. timeline:: 319830355039371264
+
 
 finally, build your sphinx project.
 
@@ -28,7 +30,7 @@ finally, build your sphinx project.
    $ make html
 '''
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = '@shomah4a'
 __license__ = 'LGPLv3'
 
